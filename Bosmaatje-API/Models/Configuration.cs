fc934bf3626205c1bf3dd9210f8f6a231e5c9969
Dimitri2004 <d.vanderhel@student.avans.nl>
2025-03-16 17:38:14 +0100

    initial commit in github

---
+++ Bosmaatje-API/Models/Configuration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Models
+{
+    public class Configuration(string childName, DateTime childBirthDate, string headDoctorName, string characterId)
+    { 
+        public string ChildName { get; set; } = childName; 
+        public DateTime ChildBirthDate { get; set; } = childBirthDate;
+        public string HeadDoctorName { get; set; } = headDoctorName; 
+        public string CharacterId { get; set; } = characterId; 
+    }
+}
