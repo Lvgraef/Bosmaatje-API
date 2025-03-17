fc2194578342e18bcc62b157940ba78c86f429ee
SamvNoort <samimiliano01@gmail.com>
2025-03-17 11:03:53 +0100

    added Identity framework, part 2

--- Bosmaatje-API/Program.cs
+++ Bosmaatje-API/Program.cs
@@ -1,9 +1,37 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("DatabaseConnection");
+var sqlConnectionStringFound = !string.IsNullOrWhiteSpace(connectionString);
+
+// Sample
+// builder.Services.AddSingleton<ICrudRepository<Guid, *other things you may need*>, *item*Repository>(_ => new *item*Repository(connectionString ?? throw new ArgumentException("No connection string found in secrets.json")));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>(options =>
+    {
+        options.User.RequireUniqueEmail = true;
+        options.Password.RequiredLength = 10;
+    })
+    .AddRoles<IdentityRole>()
+    .AddDapperStores(options =>
+    {
+        options.ConnectionString = builder.Configuration.GetConnectionString("DapperIdentity");
+    });
+
+builder.Services.AddMvc().AddJsonOptions(options =>
+    {   
+        options.JsonSerializerOptions.MaxDepth = 64;
+    });
 
 var app = builder.Build();
 
@@ -14,31 +42,19 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseAuthorization();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapGroup("/account").MapIdentityApi<IdentityUser>();
+app.MapControllers();//.RequireAuthorization();
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/", () => $"The API is up and running. Connection string found: {(sqlConnectionStringFound ? "Yes" : "No")}");
+app.MapPost("/account/logout",
+    async (SignInManager<IdentityUser> signInManager, [FromBody] object? empty) =>
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+        if (empty == null) return Results.Unauthorized();
+        await signInManager.SignOutAsync();
+        return Results.Ok();
+
+    });
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
